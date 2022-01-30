ad56b4b847ca6094ba06db72b1895831f7bb7eec
Haitian Nick <haitiannick@gmail.com>
2022-01-30 12:31:38 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can console.log(playerName, playerAttack, playerHealth);
 var enemyName = "Roborto";
@@ -43,11 +44,29 @@ var fight = function() {
       //if the player chooses to skip  
     }
     else if (promptFight === "SKIP" || promptFight === "skip") {
-        window.alert(playerName + " has chosen to skip the fight!");
+        // confirm if the player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // if yes (true), leave fight
+
+        if (confirmSkip) {
+            window.alert(playerName + " has chosen to skip the fight. Goodbye!");
+
+            // subtract money from playerMoney for skipping    
+            playerMoney = playerMoney - 2;
+        }
+        // if no (false), ask question again by running fight() again
+        else {
+            fight()
+        } 
+            
     }
     else {
         window.alert("You need to choose a valid option. Try again!");
     }
-}
+       
+} 
+
+
 
 fight();
